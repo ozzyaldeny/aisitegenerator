4168016e723659cb482bc4045bbf939678b269b9
Ozzy Aldeny <ozzy.aldeny@gmail.com>
2023-05-15 12:32:20 -0400

    validate openai key

--- AISiteBuilder.py
+++ AISiteBuilder.py
@@ -119,129 +119,132 @@ def main():
         if st.button("Set") :
             st.session_state['OpenAPIKey']=openapikey
     
-    if(st.session_state['frontend_finished']):
-        st.markdown(st.session_state['next_steps'])
-        next_steps=st.text_area('Define next steps')
-        but1, but2, remaining  = st.columns([1, 3, 5])
-        with but1:
-            if st.button("Next") :
-                code=define_nextSteps_2(st.session_state['current_web_code'],next_steps)
-                st.session_state['final_code']=code
-                st.session_state['counter'] += 1
-                
-        with but2:
-            if st.button("Finish and Deploy to NUCAL!") :
-                st.markdown("Deploying!....")
-        if('final_code' in st.session_state):
-            st.subheader("Visual View")
-            st.components.v1.html(st.session_state['final_code'][0],height=500,scrolling=True)
-            st.subheader("Backend Code")
-            st_ace(
-                        value=st.session_state['final_code'][0],
-                        language="html",
-                        theme="chaos",
-                        keybinding="vscode",
-                        font_size=14,
-                        tab_size=4,
-                        show_gutter=True,
-                        wrap=True,
-                        auto_update=False,
-                        readonly=False,
-                        min_lines=45,
-                        key=f"ace-2editor-{st.session_state['counter']}",
-                    )
-            st.subheader("Backend Code")
-            st_ace(
-                        value=st.session_state['final_code'][1],
-                        language="javascript",
-                        theme="chaos",
-                        keybinding="vscode",
-                        font_size=14,
-                        tab_size=4,
-                        show_gutter=True,
-                        wrap=True,
-                        auto_update=False,
-                        readonly=False,
-                        min_lines=45,
-                        key=f"ace-1editor-{st.session_state['counter']}",
-                    )
-            st.subheader("DB Creation")
-            st_ace(
-                        value=st.session_state['final_code'][2],
-                        language="sql",
-                        theme="chaos",
-                        keybinding="vscode",
-                        font_size=14,
-                        tab_size=4,
-                        show_gutter=True,
-                        wrap=True,
-                        auto_update=False,
-                        readonly=False,
-                        min_lines=45,
-                        key=f"ace-3editor-{st.session_state['counter']}",
-                    )
-            print(next_steps)
-
-    else:    
-        if('current_web_code' not in st.session_state): #First Site Creation
-            website_desc=st.text_area('Define web aplication you want to build','Create a homepage template using Bootstrap responsive design. Include the menu for the homepage, services, portfolio, blog, and about us on the header.')
-            if st.button("Generate Website") : 
-                # output=generate_website(website_desc)
-                output=generate_website_langchain(website_desc)
-                st.session_state['current_web_code']=output
-                st.session_state['web_code_versions']=[{'comment':website_desc  , 'code':output}]
-                print("Generate Website")
-                st.experimental_rerun()
-        else:
-            print("Printing Refine")
-            comments = [item['comment'] for item in st.session_state['web_code_versions']]
-            print(comments)
-            selected_comment = st.selectbox('Select a version to revert!', comments)
-            refine_text=st.text_area('Refine current web aplication')
-            cR1, cR2, cR3, cR4 = st.columns([1, 1, 1, 3])
-            with cR1:
-                if st.button("Revert to version!"):
-                    selected_code = next(item['code'] for item in st.session_state['web_code_versions'] if item['comment'] == selected_comment)
-                    st.session_state['current_web_code']=selected_code
-                    
-            with cR2:
-                if st.button("Regenerate!") :
-                    # newoutput=refine_website(st.session_state['current_web_code'],refine_text)
-                    newoutput=refine_website_langchain(st.session_state['current_web_code'],refine_text)
-                    st.session_state['current_web_code']=newoutput
-                    st.session_state['web_code_versions'].append({'comment':refine_text  , 'code':newoutput})
+    if('OpenAPIKey' not in st.session_state):
+        st.markdown('Please enter your OpenAPI Key to continue!')
+    else:
+        if(st.session_state['frontend_finished']):
+            st.markdown(st.session_state['next_steps'])
+            next_steps=st.text_area('Define next steps')
+            but1, but2, remaining  = st.columns([1, 3, 5])
+            with but1:
+                if st.button("Next") :
+                    code=define_nextSteps_2(st.session_state['current_web_code'],next_steps)
+                    st.session_state['final_code']=code
                     st.session_state['counter'] += 1
-                    st.experimental_rerun()
-            with cR3:
-                if st.button("I am done with the frontend, Next:"):
-                    st.session_state['frontend_finished'] = True
-                    next_steps=define_nextSteps_1(st.session_state['current_web_code'])
-                    print(next_steps)
-                    st.session_state['next_steps']=next_steps
-                    st.experimental_rerun()
-            st.text("Here is current website you created")
-            st.components.v1.html(st.session_state['current_web_code'],height=500,scrolling=True)
-            
-            c1, c2 = st.columns([3, 1])
+                    
+            with but2:
+                if st.button("Finish and Deploy to NUCAL!") :
+                    st.markdown("Deploying!....")
+            if('final_code' in st.session_state):
+                st.subheader("Visual View")
+                st.components.v1.html(st.session_state['final_code'][0],height=500,scrolling=True)
+                st.subheader("Backend Code")
+                st_ace(
+                            value=st.session_state['final_code'][0],
+                            language="html",
+                            theme="chaos",
+                            keybinding="vscode",
+                            font_size=14,
+                            tab_size=4,
+                            show_gutter=True,
+                            wrap=True,
+                            auto_update=False,
+                            readonly=False,
+                            min_lines=45,
+                            key=f"ace-2editor-{st.session_state['counter']}",
+                        )
+                st.subheader("Backend Code")
+                st_ace(
+                            value=st.session_state['final_code'][1],
+                            language="javascript",
+                            theme="chaos",
+                            keybinding="vscode",
+                            font_size=14,
+                            tab_size=4,
+                            show_gutter=True,
+                            wrap=True,
+                            auto_update=False,
+                            readonly=False,
+                            min_lines=45,
+                            key=f"ace-1editor-{st.session_state['counter']}",
+                        )
+                st.subheader("DB Creation")
+                st_ace(
+                            value=st.session_state['final_code'][2],
+                            language="sql",
+                            theme="chaos",
+                            keybinding="vscode",
+                            font_size=14,
+                            tab_size=4,
+                            show_gutter=True,
+                            wrap=True,
+                            auto_update=False,
+                            readonly=False,
+                            min_lines=45,
+                            key=f"ace-3editor-{st.session_state['counter']}",
+                        )
+                print(next_steps)
 
-            c2.subheader("Parameters")
+        else:    
+            if('current_web_code' not in st.session_state): #First Site Creation
+                website_desc=st.text_area('Define web aplication you want to build','Create a homepage template using Bootstrap responsive design. Include the menu for the homepage, services, portfolio, blog, and about us on the header.')
+                if st.button("Generate Website") : 
+                    # output=generate_website(website_desc)
+                    output=generate_website_langchain(website_desc)
+                    st.session_state['current_web_code']=output
+                    st.session_state['web_code_versions']=[{'comment':website_desc  , 'code':output}]
+                    print("Generate Website")
+                    st.experimental_rerun()
+            else:
+                print("Printing Refine")
+                comments = [item['comment'] for item in st.session_state['web_code_versions']]
+                print(comments)
+                selected_comment = st.selectbox('Select a version to revert!', comments)
+                refine_text=st.text_area('Refine current web aplication')
+                cR1, cR2, cR3, cR4 = st.columns([1, 1, 1, 3])
+                with cR1:
+                    if st.button("Revert to version!"):
+                        selected_code = next(item['code'] for item in st.session_state['web_code_versions'] if item['comment'] == selected_comment)
+                        st.session_state['current_web_code']=selected_code
+                        
+                with cR2:
+                    if st.button("Regenerate!") :
+                        # newoutput=refine_website(st.session_state['current_web_code'],refine_text)
+                        newoutput=refine_website_langchain(st.session_state['current_web_code'],refine_text)
+                        st.session_state['current_web_code']=newoutput
+                        st.session_state['web_code_versions'].append({'comment':refine_text  , 'code':newoutput})
+                        st.session_state['counter'] += 1
+                        st.experimental_rerun()
+                with cR3:
+                    if st.button("I am done with the frontend, Next:"):
+                        st.session_state['frontend_finished'] = True
+                        next_steps=define_nextSteps_1(st.session_state['current_web_code'])
+                        print(next_steps)
+                        st.session_state['next_steps']=next_steps
+                        st.experimental_rerun()
+                st.text("Here is current website you created")
+                st.components.v1.html(st.session_state['current_web_code'],height=500,scrolling=True)
+                
+                c1, c2 = st.columns([3, 1])
 
-            with c1:
-                print("Printing Editor",st.session_state['current_web_code'])
-                st.session_state['current_web_code'] = st_ace(
-                    value=st.session_state['current_web_code'],
-                    language=c2.selectbox("Language mode", ["html","vue","javascript"], index=0),
-                    theme=c2.selectbox("Theme", options=THEMES, index=1),
-                    keybinding=c2.selectbox("Keybinding mode", options=KEYBINDINGS, index=3),
-                    font_size=c2.slider("Font size", 5, 24, 14),
-                    tab_size=c2.slider("Tab size", 1, 8, 4),
-                    show_gutter=c2.checkbox("Show gutter", value=True),
-                    wrap=c2.checkbox("Wrap enabled", value=True),
-                    auto_update=c2.checkbox("Auto update", value=False),
-                    readonly=c2.checkbox("Read-only", value=False),
-                    min_lines=45,
-                    key=f"ace-editor-{st.session_state['counter']}",
-                )
+                c2.subheader("Parameters")
+
+                with c1:
+                    print("Printing Editor",st.session_state['current_web_code'])
+                    st.session_state['current_web_code'] = st_ace(
+                        value=st.session_state['current_web_code'],
+                        language=c2.selectbox("Language mode", ["html","vue","javascript"], index=0),
+                        theme=c2.selectbox("Theme", options=THEMES, index=1),
+                        keybinding=c2.selectbox("Keybinding mode", options=KEYBINDINGS, index=3),
+                        font_size=c2.slider("Font size", 5, 24, 14),
+                        tab_size=c2.slider("Tab size", 1, 8, 4),
+                        show_gutter=c2.checkbox("Show gutter", value=True),
+                        wrap=c2.checkbox("Wrap enabled", value=True),
+                        auto_update=c2.checkbox("Auto update", value=False),
+                        readonly=c2.checkbox("Read-only", value=False),
+                        min_lines=45,
+                        key=f"ace-editor-{st.session_state['counter']}",
+                    )
         
 
 if __name__ == "__main__":
